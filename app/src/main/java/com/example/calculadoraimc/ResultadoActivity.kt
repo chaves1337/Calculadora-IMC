6cd3162f4314dbe6aff8dfa93afe20626d3a488b
Matheus Sant'anna <matsnt1223@outlook.com>
2021-03-27 01:34:29 -0300

    created result activity and class to it, add images and resources values to string

---
+++ app/src/main/java/com/example/calculadoraimc/ResultadoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.calculadoraimc
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ResultadoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+    }
+}
